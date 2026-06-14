9e1138c7da3c752841f3a01503651b331299a5e3
sandip84 <sandip84@hotmail.com>
2026-06-14 17:37:40 -0400

    added sqs

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -4,3 +4,12 @@ resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-shar-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
+
+resource "aws_s3_bucket_public_access_block" "sandbox_bucket" {
+  bucket = aws_s3_bucket.sandbox_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
